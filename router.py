94e3becc42df05a2eecb83b16db971c82a4893f7
Giovani <giovanikx@gmail.com>
2018-10-17 23:10:48 -0300

    router

--- router.py
+++ router.py
@@ -9,35 +9,46 @@ import math
 from collections import defaultdict
 
 lock = threading.Lock()
+#graph_comment global DVTable,graph
 global DVTable
-global graph
 
-def _add(args, sourceIP):
-    global DVTable,graph
-    dest = args[0]
-    cost = int(args[1])
-    DVTable.append([sourceIP, cost, dest])
-    graph[sourceIP].update({dest: cost})
+
+def _add(sourceIP, cost, dest, nextStep):
+    #graph_comment global DVTable,graph
+    global DVTable
+    
+    cost = int(cost)
+    DVTable.append([sourceIP, cost, dest, nextStep])
+    # Só adicionamos na oposta caso o destino não seja igual ao próximo passo.
+    # Nesse caso, não é uma ligação direta e não sabemos se o caminho inverso é o melhor para o outro.
+    if(nextStep != dest):
+        DVTable.append([dest, cost, sourceIP, sourceIP])
+#graph_comment     graph[sourceIP].update({dest: cost})
+#graph_comment     graph[dest].update({sourceIP: cost})
     print(DVTable)
-    print(graph)
+    #graph_comment print(graph)
 
 def _del(args, sourceIP):
-    global DVTable,graph
+    #graph_comment global DVTable,graph
+    global DVTable
     dest = args[0]
     for i in range(len(DVTable)):
         if(DVTable[i][2] == dest):
-            DVTable.remove([sourceIP,DVTable[i][1],dest])
-            graph[sourceIP][dest] = math.inf
+            #DVTable.remove([sourceIP,DVTable[i][1],dest])
+            del DVTable[i]
+#graph_comment             graph[sourceIP][dest] = math.inf
 
 def _trace(args):#Enviam mensagem to tipo data e trace
-    global DVTable,graph
+    #graph_comment global DVTable,graph
+    global DVTable
     print('')
 
 def executeCommand(line, sourceIP):
     commands = line.split(" ")
     args = commands[1:]
     if(commands[0] == "add"):
-        _add(args,sourceIP)
+        # Source, custo, destino, next step.
+        _add(sourceIP, args[1], args[0], args[0])
     elif(commands[0] == "del"):
         _del(args,sourceIP)
     elif(commands[0] == "trace"):
@@ -52,7 +63,7 @@ def updateMessage(source,dest,distances):
     return update_message.encode('latin1')
 
 def dataMessage(source,dest):
-    message = {'type':'data', 'source':source, 'destination':dest, 'distances':distances}
+    message = {'type':'data', 'source':source, 'destination':dest, 'payload':'data'+'\\'+ source + '\\' + dest}
     data_message = json.dumps(message)
     #print(data_message)
     return data_message.encode('latin1')
@@ -66,7 +77,8 @@ def traceMessage():
     return trace_message.encode('latin1')
 
 def broadcastDV(TOUT,PORT): #Envia mensagem do tipo update de tempos em tempos
-    global DVTable,graph
+    #graph_comment global DVTable,graph
+    global DVTable
     broadcast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
     lock.acquire()
     distances = {dist[2]:dist[1] for dist in DVTable}
@@ -77,20 +89,66 @@ def broadcastDV(TOUT,PORT): #Envia mensagem do tipo update de tempos em tempos
     broadcast.close()
 
 def createDVTable(HOST):
-    global DVTable,graph
+    #graph_comment global DVTable,graph
+    global DVTable
     DVTable = []
     return DVTable, HOST
 
 def pathCost(sourceIP, dest):
-    global graph
+    #graph_comment global graph
     #bellmanFord algorithm
 
 def bellmanFord():
     print('')
 
 
+# Recebemos o anuncio`1             
+def update_table(js):
+    # Anunciante
+    _source = js["source"]
+
+    # Eu
+    _destination = js["destination"]
+
+    # Endereços conectados no anunciante
+    _distances = js["distances"]
+
+    # Para cada vizinho no anuncio recebido
+    for destino, custo in _distances.items():
+        # Se o destino não está na tabela
+        if(destino not in matrixDV[:,2]):
+            #graph_comment #graph.update({js['source']:{d:js['destination'][d]}})
+            _add(key,_destination[key], _source)
+        else:
+            for t in DVTable:
+                for key,val in _distances.items():
+                    if(t[2] == key):
+                        pass
+                        #Comparo para ver se o novo caminho é melhor (pathCost é o bellmanFord - caminho minimo no grafo)
+                        #if(t[1] > js['distances'][d] + pathCost(HOST,js['source']))
+
+
+
+
+
+for destino, custo in _distances.items():
+        if(key not in matrixDV[:,2]):
+            #graph_comment #graph.update({js['source']:{d:js['destination'][d]}})
+            _add(key,_destination[key], _source)
+        else:
+            for t in DVTable:
+                for key,val in _distances.items():
+                    if(t[2] == key):
+                        pass
+                        #Comparo para ver se o novo caminho é melhor (pathCost é o bellmanFord - caminho minimo no grafo)
+                        #if(t[1] > js['distances'][d] + pathCost(HOST,js['source']))
+
+
+
+
 def listen(HOST,PORT):
-    global DVTable,graph
+    #graph_comment global DVTable,graph
+    global DVTable
     listenSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
     listenSocket.bind((HOST,PORT))
     while True:
@@ -98,23 +156,14 @@ def listen(HOST,PORT):
         message = message.decode('latin1')  
         print(message)
         js = json.loads(message)
+        _type = js["type"]
         matrixDV = np.array(DVTable)
-        if(js['type'] == 'update'):
-            for key,val in js['distances'].items():
-                if(key not in matrixDV[:,2]):
-                    #graph.update({js['source']:{d:js['destination'][d]}})
-                    _add([key,js['destination'][key]], js['source'])
-                else:
-                    for t in DVTable:
-                        for key,val in js['distances'].items():
-                            if(t[2] == key):
-                                pass
-                                #Comparo para ver se o novo caminho é melhor (pathCost é o bellmanFord - caminho minimo no grafo)
-                                #if(t[1] > js['distances'][d] + pathCost(HOST,js['source']))
-
-        elif(js['type'] == 'data'):
+        if(_type == 'update'):
+            update_table(js)
+            
+        elif(_type == 'data'):
             pass
-        elif(js['type'] == 'trace'):
+        elif(_type == 'trace'):
             pass
          
 
@@ -134,14 +183,15 @@ def CLI(sourceIP):
 
 def main():
     global DVTable
-    global graph
+    global DVTable
+    #graph_comment global graph
     t_start = time.time()
     param = sys.argv[1:] 
     PORT = 55151
     HOST = param[0]
     period = param[1]
     DVTable = [[HOST,0,HOST]]
-    graph = defaultdict(dict)
+#graph_comment     graph = defaultdict(dict)
 
     if(len(param) > 2):#Startup
         filename = param[2]
